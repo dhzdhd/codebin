d9c026d4d67e7632ff888dc7a24a7a88ef262815
dhzdhd <ragas5432100@gmail.com>
2022-09-24 18:47:21 +0530

    first commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,52 @@
+<nav>
+    <ul>
+        <li>
+            T
+        </li>
+        <li>
+            T
+        </li>
+        <li>
+            T
+        </li>
+        <li>
+            T
+        </li>
+        <li>
+            T
+        </li>
+        <li>
+            T
+        </li>
+    </ul>
+</nav>
+
+<style lang="sass">
+    nav
+        position: absolute
+        top: 50%
+        transform: translate(0, -50%)
+        width: 4rem
+        margin-left: 1.5rem
+        background-color: white
+        border-radius: 2rem
+
+        ul
+            display: flex
+            flex-direction: column
+            align-items: center
+            padding: 1rem 0
+
+            li
+                display: grid
+                place-content: center
+                background-color: white
+                color: #7694c9
+                aspect-ratio: 1
+                width: 3rem
+                border-radius: 100%
+
+                &:hover
+                    background-color: #00288a
+                    color: white
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Navbar from "../components/Navbar.svelte";
+    import '../styles.sass'
+</script>
+
+<header></header>
+<Navbar />
+<main><slot></slot></main>
+
+<style lang="sass">
+    @use 'sass:color'
+    @use '../styles'
+
+    :root
+        background-color: #ecf7ff
+</style>
