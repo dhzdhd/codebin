213b8455271abe778dd197da7a7f8cd3f8a6ca01
dhzdhd <ragas5432100@gmail.com>
2022-09-28 17:47:08 +0530

    Add express TS server

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,29 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import express from 'express';
+import fs from 'fs';
+import cors = require('cors');
+import { join } from 'path';
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+const app = express();
+const PORT = process.env.PORT || 5000
+
+const allowedOrigins = ['http://localhost:5173', 'https://codebin-dun.vercel.app/'];
+const options = {
+  origin: allowedOrigins
 };
+
+app.use(cors(options));
+
+app.get('/api', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+
+    res.json({
+      a: 'e'
+    });
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on http://localhost:5000/');
+})
+
+module.exports = app;
