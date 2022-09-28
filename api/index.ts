cfbc291b704d9b78f64fb908ca8140f7d6a8ad4e
dhzdhd <ragas5432100@gmail.com>
2022-09-29 01:27:00 +0530

    Add firebase to api

--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,67 @@
 import express from 'express';
-import fs from 'fs';
 import cors = require('cors');
-import { join } from 'path';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, set, ref } from 'firebase/database';
+
+const firebaseConfig = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
+	measurementId: process.env.MEASUREMENT_ID
+};
+
+const fbApp = initializeApp(firebaseConfig);
+const database = getDatabase(fbApp);
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT ?? 5000;
 
 const allowedOrigins = ['http://localhost:5173', 'https://codebin-dun.vercel.app/'];
 const options = {
-  origin: allowedOrigins
+	origin: allowedOrigins
 };
 
 app.use(cors(options));
 
 app.get('/api', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+	res.type('text');
 
-    res.json({
-      a: 'e'
-    });
-})
+  res.send('Paste API')
+});
+
+app.get('/api/pastes', async (req, res) => {
+	res.type('json');
+});
+
+app
+	.route('/api/pastes/:id')
+	.post(async (req, res) => {
+    res.type('json');
+		const id = req.params['id'];
+
+		set(ref(database, 'pastes/' + id), {
+			content: req.body
+		});
+
+		res.json({
+			id: id
+		});
+	})
+	.get(async (req, res) => {
+    res.type('json');
+		const id = req.params['id'];
+
+		res.json({
+			id: id
+		});
+	});
 
 app.listen(PORT, () => {
-    console.log('Listening on http://localhost:5000/');
-})
+	console.log('Listening on http://localhost:5000/');
+});
 
 module.exports = app;
